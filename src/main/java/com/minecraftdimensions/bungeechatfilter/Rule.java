df5ed70f57cd13cbd90309bbe4aa0bacbd5d0973
leonfagan71 <leonfagan71@googlemail.com>
2017-04-10 15:31:34 +0100

    Replaced tabs with spaces

--- src/main/java/com/minecraftdimensions/bungeechatfilter/Rule.java
+++ src/main/java/com/minecraftdimensions/bungeechatfilter/Rule.java
@@ -12,137 +12,137 @@ import java.util.regex.Pattern;
 
 public class Rule {
 
-	Pattern regex;
-	Pattern ignore;
-	HashMap<String, String[]> actions;
-	String permission = null;
-	boolean needsPerm;
+    Pattern regex;
+    Pattern ignore;
+    HashMap<String, String[]> actions;
+    String permission = null;
+    boolean needsPerm;
 
-	public Rule(String regex, HashMap<String, String[]> actions, String permission, String ignores) {
-		this.regex = Pattern.compile(regex);
-		if (ignores == null) {
-			ignore = null;
-		} else {
-			this.ignore = Pattern.compile(ignores);
-		}
-		this.actions = actions;
-		if (permission != null && permission.startsWith("!")) {
-			permission = permission.substring(1, permission.length());
-			needsPerm = true;
-		}
-		this.permission = permission;
-	}
+    public Rule(String regex, HashMap<String, String[]> actions, String permission, String ignores) {
+        this.regex = Pattern.compile(regex);
+        if (ignores == null) {
+            ignore = null;
+        } else {
+            this.ignore = Pattern.compile(ignores);
+        }
+        this.actions = actions;
+        if (permission != null && permission.startsWith("!")) {
+            permission = permission.substring(1, permission.length());
+            needsPerm = true;
+        }
+        this.permission = permission;
+    }
 
-	public Pattern getRegex() {
-		return regex;
-	}
+    public Pattern getRegex() {
+        return regex;
+    }
 
-	public String getStringRegex() {
-		return regex.pattern();
-	}
+    public String getStringRegex() {
+        return regex.pattern();
+    }
 
-	public Matcher getMatcher(String message) {
-		return regex.matcher(message);
-	}
+    public Matcher getMatcher(String message) {
+        return regex.matcher(message);
+    }
 
-	public boolean doesMessageContainRegex(String message) {
-		return getMatcher(message).find();
-	}
+    public boolean doesMessageContainRegex(String message) {
+        return getMatcher(message).find();
+    }
 
-	public void performActions(ChatEvent event, ProxiedPlayer player) {
-		String message = event.getMessage();
-		if (ignore != null) {
-			Matcher ig = Pattern.compile(ignore.pattern()).matcher(message);
-			while (ig.find()) {
-				return;
-			}
-		}
-		for (String action : actions.keySet()) {
-			if (action.equals("deny")) {
-				event.setCancelled(true);
-			} else if (action.equals("message")) {
-				String rp = customParams(actions.get(action)[0], player, event);
-				player.sendMessage(TextComponent.fromLegacyText(Main.color(rp)));
-			} else if (action.equals("kick")) {
-				String rp = customParams(actions.get(action)[0], player, event);
-				player.disconnect(new TextComponent(TextComponent.fromLegacyText(Main.color(rp))));
-			} else if (action.equals("alert")) {
-				String rp = customParams(actions.get(action)[0], player, event);
-				if (message.split(" ", 2).length > 1) {
-					rp = rp.replace("{arguments}", message.split(" ", 2)[1]);
-				}
-				ProxyServer.getInstance().broadcast(new TextComponent(Main.color(rp)));
-			} else if (action.equals("scommand")) {
-				String rp = customParams(actions.get(action)[0], player, event);
-				player.chat(rp);
-			} else if (action.equals("pcommand")) {
-				String rp = customParams(actions.get(action)[0], player, event);
-				ProxyServer.getInstance().getPluginManager().dispatchCommand(player, rp);
-			} else if (action.equals("ccommand")) {
-				String rp = customParams(actions.get(action)[0], player, event);
-				ProxyServer.getInstance().getPluginManager().dispatchCommand(ProxyServer.getInstance().getConsole(), rp);
-			} else if (action.equals("remove")) {
-				message = message.replaceAll(regex.pattern(), "");
-			} else if (action.equals("replace")) {
-				Random rand = new Random();
-				Matcher m = getMatcher(message);
-				StringBuilder sb = new StringBuilder();
-				int last = 0;
-				while (m.find()) {
-					int n = rand.nextInt(actions.get(action).length);
-					sb.append(message.substring(last, m.start()));
-					sb.append(actions.get(action)[n]);
-					last = m.end();
-				}
-				sb.append(message.substring(last));
-				message = sb.toString();
-			} else if (action.equals("lower")) {
-				Matcher m = getMatcher(message);
-				StringBuilder sb = new StringBuilder();
-				int last = 0;
-				while (m.find()) {
-					sb.append(message.substring(last, m.start()));
-					sb.append(m.group(0).toLowerCase());
-					last = m.end();
-				}
-				sb.append(message.substring(last));
-				message = sb.toString();
-			}
-		}
-		event.setMessage(message);
-	}
+    public void performActions(ChatEvent event, ProxiedPlayer player) {
+        String message = event.getMessage();
+        if (ignore != null) {
+            Matcher ig = Pattern.compile(ignore.pattern()).matcher(message);
+            while (ig.find()) {
+                return;
+            }
+        }
+        for (String action : actions.keySet()) {
+            if (action.equals("deny")) {
+                event.setCancelled(true);
+            } else if (action.equals("message")) {
+                String rp = customParams(actions.get(action)[0], player, event);
+                player.sendMessage(TextComponent.fromLegacyText(Main.color(rp)));
+            } else if (action.equals("kick")) {
+                String rp = customParams(actions.get(action)[0], player, event);
+                player.disconnect(new TextComponent(TextComponent.fromLegacyText(Main.color(rp))));
+            } else if (action.equals("alert")) {
+                String rp = customParams(actions.get(action)[0], player, event);
+                if (message.split(" ", 2).length > 1) {
+                    rp = rp.replace("{arguments}", message.split(" ", 2)[1]);
+                }
+                ProxyServer.getInstance().broadcast(new TextComponent(Main.color(rp)));
+            } else if (action.equals("scommand")) {
+                String rp = customParams(actions.get(action)[0], player, event);
+                player.chat(rp);
+            } else if (action.equals("pcommand")) {
+                String rp = customParams(actions.get(action)[0], player, event);
+                ProxyServer.getInstance().getPluginManager().dispatchCommand(player, rp);
+            } else if (action.equals("ccommand")) {
+                String rp = customParams(actions.get(action)[0], player, event);
+                ProxyServer.getInstance().getPluginManager().dispatchCommand(ProxyServer.getInstance().getConsole(), rp);
+            } else if (action.equals("remove")) {
+                message = message.replaceAll(regex.pattern(), "");
+            } else if (action.equals("replace")) {
+                Random rand = new Random();
+                Matcher m = getMatcher(message);
+                StringBuilder sb = new StringBuilder();
+                int last = 0;
+                while (m.find()) {
+                    int n = rand.nextInt(actions.get(action).length);
+                    sb.append(message.substring(last, m.start()));
+                    sb.append(actions.get(action)[n]);
+                    last = m.end();
+                }
+                sb.append(message.substring(last));
+                message = sb.toString();
+            } else if (action.equals("lower")) {
+                Matcher m = getMatcher(message);
+                StringBuilder sb = new StringBuilder();
+                int last = 0;
+                while (m.find()) {
+                    sb.append(message.substring(last, m.start()));
+                    sb.append(m.group(0).toLowerCase());
+                    last = m.end();
+                }
+                sb.append(message.substring(last));
+                message = sb.toString();
+            }
+        }
+        event.setMessage(message);
+    }
 
-	public boolean hasPermission() {
-		return permission != null;
-	}
+    public boolean hasPermission() {
+        return permission != null;
+    }
 
-	public boolean needsPermission() {
-		return needsPerm;
-	}
+    public boolean needsPermission() {
+        return needsPerm;
+    }
 
-	public String getPermission() {
-		return permission;
-	}
+    public String getPermission() {
+        return permission;
+    }
 
-	public String findRWord(String message, String regex) throws java.lang.IllegalStateException {
-		Pattern pattern = Pattern.compile(regex);
-		Matcher m = pattern.matcher(message);
-		m.find();
-		return m.group(1);
-	}
+    public String findRWord(String message, String regex) throws java.lang.IllegalStateException {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher m = pattern.matcher(message);
+        m.find();
+        return m.group(1);
+    }
 
-	public String customParams(String configMessage, ProxiedPlayer player, ChatEvent event) {
-		String chatMessage = event.getMessage();
-		String ret = "";
-		String match = "NULL";
-		try {
-			match = findRWord(chatMessage, this.regex.pattern());
-		} catch (Exception e) {
-		}
-		ret = configMessage
-				.replace("{player}", player.getName())
-				.replace("{message}", chatMessage)
-				.replace("{match}", match);
-		return ret;
-	}
+    public String customParams(String configMessage, ProxiedPlayer player, ChatEvent event) {
+        String chatMessage = event.getMessage();
+        String ret = "";
+        String match = "NULL";
+        try {
+            match = findRWord(chatMessage, this.regex.pattern());
+        } catch (Exception e) {
+        }
+        ret = configMessage
+                .replace("{player}", player.getName())
+                .replace("{message}", chatMessage)
+                .replace("{match}", match);
+        return ret;
+    }
 }
